ca51be14e0c0578bec7ccca251bbd9ab87f8afed
cwang3 <cwang3@MISLW5A46A2C8D>
2021-06-19 19:01:43 -0400

    Update gunicorn_conf.py

--- app/gunicorn_conf.py
+++ app/gunicorn_conf.py
@@ -1,4 +1,7 @@
-import multiprocessing
+import multiprocessing, os
 
 bind = "0.0.0.0:80"
+port = os.getenv('PORT')
+if port != None:
+    bind = "0.0.0.0:" + port
 workers = multiprocessing.cpu_count() * 2 + 1
